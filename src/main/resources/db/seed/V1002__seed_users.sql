e834130ed1356200f8804112b58feb1e75b0cce3
Connor Burge <connor.d.burge@gmail.com>
2025-08-09 15:23:28 -0400

    connors solution to mock user context locally

--- src/main/resources/db/seed/V1002__seed_users.sql
+++ src/main/resources/db/seed/V1002__seed_users.sql
@@ -4,6 +4,6 @@
 
 INSERT INTO users (id, created_at, updated_at, email, password, name, is_active, last_workspace_id)
 VALUES
-    ('${user1_id}', NOW(), NOW(), '${user1_email}', '${password_hash}', '${user1_name}', true, '${user1_workspace_id}'),
-    ('${user2_id}', NOW(), NOW(), '${user2_email}', '${password_hash}', '${user2_name}', true, '${user2_workspace_id}')
+    ('${user1_id}', NOW(), NOW(), '${user1_email}', '${user1_password}', '${user1_name}', true, '${user1_workspace_id}'),
+    ('${user2_id}', NOW(), NOW(), '${user2_email}', '${user2_password}', '${user2_name}', true, '${user2_workspace_id}')
 ON CONFLICT (id) DO NOTHING;
